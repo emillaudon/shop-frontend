2dce0717da2cfcb2646d67562f63e08dd8883616
emillaudon <emil.laudn@gmail.com>
2026-01-06 22:48:25 +0100

    add initial products page + routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TopbarComponent } from './components/topbar/topbar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TopbarComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
+import { ProductsComponent } from './components/products/products.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent }
+    { path: 'products', component: ProductsComponent },
+    { path: '', redirectTo: 'products', pathMatch: 'full' }
 ];
