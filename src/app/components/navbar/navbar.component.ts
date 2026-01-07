af020be7b840cb1b012cec6e5026732513ff4022
emillaudon <emil.laudn@gmail.com>
2026-01-07 16:12:47 +0100

    clean up unused code

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { SearchFieldComponent } from '../search-field/search-field.component';
 import { NavbarTextButtonComponent } from '../navbar-text-button/navbar-text-button.component';
 import { CartButtonComponent } from "../cart-button/cart-button.component";
-import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [SearchFieldComponent, NavbarTextButtonComponent, CartButtonComponent, RouterLink],
+  imports: [SearchFieldComponent, NavbarTextButtonComponent, CartButtonComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
