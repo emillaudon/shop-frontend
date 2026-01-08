799f9ed94ba59ddd9f9092901e0ef5e8a0b023ea
emillaudon <emil.laudn@gmail.com>
2026-01-08 17:33:12 +0100

    add products grid + implement it loading product cards from hardcoded products

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -4,6 +4,7 @@ import { Product } from '../../models/product';
 @Component({
   selector: 'app-product-card',
   imports: [],
+  standalone: true,
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.scss'
 })
--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { ProductCardComponent } from '../product-card/product-card.component';
 import { Product } from '../../models/product';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-products',
-  imports: [ProductCardComponent],
+  standalone: true,
+  imports: [ProductCardComponent, NgFor],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
 export class ProductsComponent {
+  products: Product[] = [
+    new Product(2, "Other Shirt", 10, 10),
+    new Product(3, "Placeholder", 4, 4),
+    new Product(4, "Also Placeholder", 10,10),
+    new Product(5, "Also Placeholder 2", 11,11),
+    new Product(6, "Other Shirt", 10, 10),
+    new Product(7, "Placeholder", 4, 4),
+    new Product(8, "Also Placeholder", 10,10),
+    new Product(9, "Also Placeholder 2", 11,11),
+
+  ];
   product = new Product(1, "T-Shirt", 4, 10);
+  trackById = (_: number, p: Product) => p.id;
 }
