45ff39b6d77f7e53edfde55929ddc40ea9d48cab
emillaudon <emil.laudn@gmail.com>
2026-01-09 23:50:07 +0100

    implement increase, decrease & remove items from cart service

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -53,6 +53,36 @@ export class CartService {
     ]);
   };
 
+  decreaseAmount(productId: number) {
+    const items = this.itemsSubject.value;
+
+    const updated = items.map(i => 
+      i.productId === productId
+      ? { ...i, quantity: i.quantity - 1 }
+      : i
+    ).filter(i => i.quantity > 0);
+    this.itemsSubject.next(updated);
+  }
+
+  increaseAmount(productId: number) {
+    const items = this.itemsSubject.value;
+
+    const updated = items.map(i => 
+      i.productId === productId
+      ? { ...i, quantity: i.quantity - 1 }
+      : i
+    );
+    this.itemsSubject.next(updated);
+  }
+
+  remove(productId: number) {
+    const items = this.itemsSubject.value;
+
+    const updated = items.filter(i => i.productId === productId);
+    
+    this.itemsSubject.next(updated);
+  }
+
   clear() {
     this.itemsSubject.next([]);
   }
