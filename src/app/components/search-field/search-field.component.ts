125b9ec4c64e3dc20c1ebb92111140231a04542b
emillaudon <emil.laudn@gmail.com>
2026-01-11 17:08:03 +0100

    implement clearing search on going backwards / forwards

--- src/app/components/search-field/search-field.component.ts
+++ src/app/components/search-field/search-field.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { NavigationStart, Router } from '@angular/router';
+import { filter, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-search-field',
@@ -9,7 +11,21 @@ import { FormsModule } from '@angular/forms';
 })
 export class SearchFieldComponent {
   @Output() search = new EventEmitter<string>();
+  private sub = new Subscription();
+
   value = '';
+
+  constructor(private router: Router) {
+    this.sub.add(
+      this.router.events.pipe(
+        filter((e): e is NavigationStart => e instanceof NavigationStart),
+
+        filter(e => e.navigationTrigger === 'popstate')
+      ).subscribe(() => {
+        this.clear()
+      })
+    );
+  }
 
   submit() {
     this.search.emit(this.value.trim());
