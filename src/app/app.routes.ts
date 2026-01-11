dbbef0ac933707950a07302a7b842721ea11d596
emillaudon <emil.laudn@gmail.com>
2026-01-11 18:37:16 +0100

    add initial orders page + order card

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
 import { ProductsComponent } from './components/products/products.component';
-import { HomeComponent } from './pages/home/home.component';
+import { OrdersComponent } from './components/orders/orders.component';
 
 export const routes: Routes = [
     { path: 'products', component: ProductsComponent },
     { path: '', redirectTo: 'products', pathMatch: 'full' },
-    { path: 'home', component: HomeComponent }
+    { path: 'orders', component: OrdersComponent }
 ];
---
+++ src/app/components/orders/orders.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { OrderCardComponent } from "../order-card/order-card.component";
+
+@Component({
+  selector: 'app-orders',
+  imports: [OrderCardComponent],
+  templateUrl: './orders.component.html',
+  styleUrl: './orders.component.scss'
+})
+export class OrdersComponent {
+
+}
